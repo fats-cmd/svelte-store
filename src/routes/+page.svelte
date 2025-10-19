75e70c18d32d06d35c9ba82a1a7c85e52437f87f
ronald <okpararonald0@gmail.com>
2025-10-19 12:39:43 -0700

    i lay my love on you

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Header name="ronald" age={30} date_of_birth="1993-05-12" email="ronald@example.com" />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let {
+		name,
+		age,
+		date_of_birth,
+		email
+	}: {
+		name: string;
+		age: number;
+		date_of_birth: string;
+		email: string;
+	} = $props();
+</script>
+
+<h1>thi s a header</h1>
+
+<style></style>
